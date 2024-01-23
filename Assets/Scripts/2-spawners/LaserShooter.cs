c490a3e3dd0da60fac4dad23d7b6dc62cf865348
asaftk1 <93122554+asaftk1@users.noreply.github.com>
2024-01-24 00:11:16 +0200

    Ex3
    commit

---
+++ Assets/Scripts/2-spawners/LaserShooter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/**
+ * This component spawns the given laser-prefab whenever the player clicks a given key.
+ * It also updates the "scoreText" field of the new laser.
+ */
+public class LaserShooter : ClickSpawner
+{
+    [SerializeField] NumberField scoreField;
+    [SerializeField] float cooldownTime = 1f;
+
+    private bool canShoot = true;
+
+    protected override GameObject spawnObject()
+    {
+        if (canShoot)
+        {
+            GameObject newObject = base.spawnObject();  // base = super
+
+            // Modify the text field of the new object.
+            ScoreAdder newObjectScoreAdder = newObject.GetComponent<ScoreAdder>();
+
+            if (newObjectScoreAdder)
+                newObjectScoreAdder.SetScoreField(scoreField);
+            StartCoroutine(StartCooldown());
+
+            return newObject;
+        }
+        return null;
+    }
+    IEnumerator StartCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(cooldownTime);
+        canShoot = true;
+    }
+}
---
+++ Assets/Scripts/3-collisions/ScoreAdder.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+/*
+ * This component increases a given "score" field whenever it is triggered.
+ */
+
+public class ScoreAdder : MonoBehaviour
+{
+    [Tooltip("Every object tagged with this tag will trigger adding score to the score field.")]
+    [SerializeField] string triggeringTag;
+    [SerializeField] NumberField scoreField;
+    public int pointsToAdd = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && scoreField != null)
+        {
+
+
+            if ((scoreField.GetNumber() % 50) == 0 && scoreField.GetNumber() != 0)
+            {
+                scoreField.AddNumber(pointsToAdd * 10);
+
+            }
+            else
+            {
+                scoreField.AddNumber(pointsToAdd);
+            }
+        }
+    }
+
+    public void SetScoreField(NumberField newTextField)
+    {
+        this.scoreField = newTextField;
+    }
+
+}
